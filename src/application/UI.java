439e75b98252063d4f6018dadd8647e9d9f5fb87
Moises Pedro <moises_mpos@hotmail.com>
2023-04-18 14:54:17 -0300

    Logica do xequemate

--- src/application/UI.java
+++ src/application/UI.java
@@ -51,9 +51,15 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.GetTurn());
-		System.out.println("Waiting player: " + chessMatch.GetCurrentPlayer());
-		if (chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.GetCurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		}
+		else {
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: " + chessMatch.GetCurrentPlayer());
 		}
 	}
 
